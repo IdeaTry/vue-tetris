ae56446ec106deb5d71e8497b80c89afe829aba1
liuhaiping <mailhap@qq.com>
2016-09-08 15:41:42 +0800

    添加旋转功能；生成随机形状；添加1型和+型形状；

--- src/page/index/shape.js
+++ src/page/index/shape.js
@@ -1,13 +1,18 @@
-
 import Rotator from './shape-rotator.js'
 
+// 预置的一些形状
 const shapes = {
 	'L': [[0,0],[0,1],[0,2],[1,2]],
-	'T': [[0,0],[1,0],[2,0],[1,1],[1,2]],
+	'T': [[0,0],[1,0],[2,0],[1,1]],
 	'7': [[1,0],[2,0],[2,1],[2,2]],
-	'1': [[1,0],[1,1],[1,2]]
+	'1': [[1,0],[1,1],[1,2]],
+	'+': [[0,1],[1,0],[1,1],[2,1],[1,2]],
 };
 
+// 所有预置的形状的key
+const shapeKeys = Object.keys(shapes); 
+
+// 形状对象，包含若干个格子，可以移动或者旋转，可以复制
 function Shape(array){
 	if(array){
 		this.set(array);
@@ -15,31 +20,44 @@ function Shape(array){
 	this.rotator = new Rotator({
 		width: 3
 	});
+	this.x = 0;
+	this.y = 0;
 }
 
 Shape.prototype = {
+
+	// 用一个二维数组来设定形状的所有的格子
 	set: function(array){
-		if(!array || !array.length) throw 'need array';
+		if(!array) throw 'need array';
 		this._array = JSON.parse(JSON.stringify(array));
 		this._array.forEach(function(n,i){
 			this[i] = n;
 		}.bind(this));
-	},
-	forEach: function(fn){
-		this._array.forEach(fn);
+
 		return this;
 	},
-	offset: function(offset){
-		if(arguments.length === 0){
-			
-		};
 
+	// 遍历格子
+	forEach: function(fn){
+		this._array.forEach(fn);
+
+		return this;
+	},
+
+	// 对形状（的所有格子）进行位移
+	offset: function(offset){
+		this.x += offset[0];
+		this.y += offset[1];
+		//
 		this._array.forEach(function(n){
 			n[0] += offset[0];
 			n[1] += offset[1];
 		});
+
 		return this;
 	},
+
+	// 向左、右，或者向下移动
 	move: function(d){
 		if(d === 'left'){
 			this.prev = this.toJson();
@@ -53,14 +71,36 @@ Shape.prototype = {
 		}else{
 			throw 'except "left" or "right"';
 		};
+
 		return this;
 	},
-	roate: function(){console.info(this.toJson())
-		this.rotator.left(this.toJson());
+
+	// 旋转（向左）
+	roate: function(){
+		var clone = this.clone(), 
+			x = this.x, 
+			y = this.y;
+		//
+		clone.offset([-x, -y]);
+		clone.set(this.rotator.left(clone.toJson()));
+		clone.offset([ x,  y]);
+		//
+		this.prev = this.toJson();
+		this.set(clone.toJson());
+
+		return this;
 	},
+
+	// 克隆一个形状，可用于在移动和旋转之前的边缘和阻碍的检测
 	clone: function(){
-		return new Shape(this.toJson());
+		var clone = new Shape(this.toJson());
+		clone.x = this.x;
+		clone.y = this.y;
+
+		return clone;
 	},
+
+	// 跟另外一个形状进行比较，并返回比较对象所没有的点，用于清除轨迹
 	specific: function(shape){
 		var map = this.toMap();
 		var specific = [];
@@ -70,23 +110,27 @@ Shape.prototype = {
 		Object.keys(map).forEach(function(n){
 			specific.push(map[n]);
 		});
+
 		return specific;
 	},
+
+	// 转成纯数组格式的数据，便于计算
 	toJson: function(){
 		return JSON.parse(JSON.stringify(this._array));
 	},
+
+	// 转成键值对，便于计算
 	toMap: function(){
 		var map = {};
 		this.forEach(function(n){
 			map[n.toString()] = n;
 		});
-		return map;
-	},
-	setStage: function(stage){
 
+		return map;
 	}
 };
 
+// 创建一个指定的形状
 Shape.create = function(type){
 	var shape;
 	//
@@ -98,6 +142,12 @@ Shape.create = function(type){
 	}else{
 		throw 'wrong shape "' + type + '"';
 	};
+};
+
+// 创建一个随机的形状
+Shape.random = function(){
+	var type = shapeKeys[parseInt(shapeKeys.length*Math.random(),10)];
+	return Shape.create(type);
 };
 
 export default Shape;
