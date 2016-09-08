2f511b48ba7dd6441d592f2436172a15e68f3888
liuhaiping <mailhap@qq.com>
2016-09-08 15:42:04 +0800

    支持向左旋转

--- src/page/index/shape-rotator.js
+++ src/page/index/shape-rotator.js
@@ -1,38 +1,64 @@
+// 对二维数组的形状进行旋转的工具类
 var Rotator = function(opt){
 	opt = opt || {};
-	opt.width = opt.width || 3;
+	this.width = opt.width || 3;
 };
 
 Rotator.prototype = {
+
+	// 对一个形状的二维数组进行向左旋转
 	left: function(shape){
-		var matrix = shapeToMatrix(shape);
-		console.info(matrix);
+		var matrix = this.shapeToMatrix(shape, this.width);
+		var rows = [], row, w = this.width;
+
+		for(var j = 0; j < w; j++){
+			row = [];
+			for(var i = w - 1; i >= 0; i--){
+				row.push(matrix[i][j]);
+			};
+			rows.push(row);
+		};
+		
+		return this.matrixToShape(rows);
 	},
-	right: function(shape){
 
+	// 把图形转成矩阵
+	shapeToMatrix: function(shape){
+		var matrix = this.createMatrix();
+		shape.forEach(function(n){
+			var x = n[0], y = n[1];
+			if(matrix[x] && matrix[x][y]){
+				matrix[x][y] = 'yes';
+			};
+		});
+		return matrix;
+	},
+
+	// 把矩阵转成图形
+	matrixToShape: function(matrix){
+		var shape = [];
+		matrix.forEach(function(col, x){
+			col.forEach(function(cell, y){
+				if(cell === 'yes'){
+					shape.push([x, y]);
+				};
+			});
+		});
+		return shape;
+	},
+
+	// 创建一个空的矩阵
+	createMatrix: function(){
+		var matrix = [], row;
+		for(var i = 0; i < this.width; i++){
+			row = [];
+			for(var j = 0; j < this.width; j++){
+				row.push('no');
+			};
+			matrix.push(row);
+		};
+		return matrix;
 	}
-};
-
-function shapeToMatrix(shape, width){
-	var matrix = createMatrix(width);
-	shape.forEach(function(n){
-		var i = n[0], j = n[1];
-		if(matrix[i] && matrix[i][j]){
-			matrix[i][j] = 'yes';
-		};
-	});
-};
-
-function createMatrix(width){
-	var matrix = [], row;
-	for(var i = 0; i < width; i++){
-		row = [];
-		for(var j = 0; j < width; j++){
-			row.push('no');
-		};
-		matrix.push(row);
-	};
-	return matrix;
 };
 
 export default Rotator;
